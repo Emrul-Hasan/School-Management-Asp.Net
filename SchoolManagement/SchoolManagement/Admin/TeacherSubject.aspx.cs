802780902818e9e9004910bc5200ee6c9abfc792
Emrul-Hasan <72728517+Emrul-Hasan@users.noreply.github.com>
2023-07-14 23:48:26 +0600

    TeacherSubject model

--- SchoolManagement/SchoolManagement/Admin/TeacherSubject.aspx.cs
+++ SchoolManagement/SchoolManagement/Admin/TeacherSubject.aspx.cs
@@ -1,15 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using static SchoolManagement.Models.CommonFn;
 
 namespace SchoolManagement.Admin
 {
     public partial class TeacherSubject : System.Web.UI.Page
     {
+        Commonfnx fn = new Commonfnx();
         protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GetClass()
+        {
+            DataTable dt = fn.Fetch("Select * from Class");
+            ddlClass.DataSource = dt;
+            ddlClass.DataTextField = "ClassName";
+            ddlClass.DataValueField = "ClassId";
+            ddlClass.DataBind();
+            ddlClass.Items.Insert(0, "Select Class");
+
+        }
+
+        protected void ddlClass_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string classId = ddlClass.SelectedValue;
+            DataTable dt = fn.Fetch("Select * from Subject where ClassId = '"+classId+"'");
+            ddlSubject.DataSource = dt;
+            ddlSubject.DataTextField = "SubjectName";
+            ddlSubject.DataValueField = "SubjectId";
+            ddlSubject.DataBind();
+            ddlSubject.Items.Insert(0, "Select Subject");
+
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
         {
 
         }
