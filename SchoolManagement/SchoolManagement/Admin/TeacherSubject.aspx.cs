0ca5a0b54cb7917ce52d4d8d03368c9d2aec1d70
Emrul-Hasan <72728517+Emrul-Hasan@users.noreply.github.com>
2023-07-16 00:20:58 +0600

    TeacherSubject Bug Fixed

--- SchoolManagement/SchoolManagement/Admin/TeacherSubject.aspx.cs
+++ SchoolManagement/SchoolManagement/Admin/TeacherSubject.aspx.cs
@@ -184,7 +184,13 @@ namespace SchoolManagement.Admin
                     ddlSubject.DataTextField = "SubjectName";
                     ddlSubject.DataValueField = "SubjectId";
                     ddlSubject.DataBind();
-                   // ddlSubject.Items.Insert(0, "Select Subject");
+                    ddlSubject.Items.Insert(0, "Select Subject");
+                    string teacherSubjectId = GridView1.DataKeys[e.Row.RowIndex].Value.ToString();
+                    DataTable dataTable = fn.Fetch(@"Select ts.Id, ts.ClassId, 
+                                 ts.SubjectId, s.SubjectName from TeacherSubject ts
+                                 inner join Subject s on ts.SubjectId = s.SubjectId where ts.Id = '"+ teacherSubjectId +"'");
+                    ddlSubject.SelectedValue = dataTable.Rows[0]["SubjectId"].ToString();
+
 
                 }
             }
